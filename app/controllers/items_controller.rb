62fe8387429e65ce3f5f49a597dc49126b4ef9f4
fish4173 <uoji4173@gmail.com>
2023-06-04 19:27:40 +0900

    商品情報編集機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+  before_action :set_prototype, only: [:show, :edit, :update]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,17 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
+
     redirect_to root_path unless current_user == @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if current_user == @item.user && @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -43,4 +43,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :shipping_duration_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @item = Item.find(params[:id])
+  end
+
 end
